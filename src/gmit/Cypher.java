71b52e47ff16200379987ef93b71daff3b21f400
Ross Byrne <ross.byrne@outlook.ie>
2015-03-12 18:25:35 +0000

    Another quick update
    added comment outlining plan of action

--- src/gmit/Cypher.java
+++ src/gmit/Cypher.java
@@ -6,6 +6,23 @@ package gmit;
 
 public class Cypher {
 
-	
+	/*
+	 *  To encrypt whole string (after reading in) use this sample code
+	 *  
+	 *  String s = "...stuff...";
+
+		for (int i = 0; i < s.length(); i++){
+		    char c = s.charAt(i);        
+		    
+		     *  encrypt character
+		     * 
+		     * use key word here
+		     * to jumble order of encrypted text
+		     * 
+		     * use string builder here to
+		     * rebuild encripted text as string
+		     *  
+		}
+	 */
 	
 } // Class
