4f5401c06418ff45a0e801e93dc57974a89024c2
Ross Byrne <ross.byrne@outlook.ie>
2015-03-12 17:07:50 +0000

    First Commit
    Just getting things set up

---
+++ src/gmit/Cypher.java
@@ -0,0 +1,11 @@
+/*
+ * All of the encryption happens here. 
+ */
+
+package gmit;
+
+public class Cypher {
+
+	
+	
+} // Class
---
+++ src/gmit/MainClass.java
@@ -0,0 +1,20 @@
+/*
+ * Author: Ross Byrne 2015
+ * My version of German ADFGVX Cypher
+ * I added two extra letters, E and T
+ * to cover extra special characters
+ * */
+
+package gmit;
+
+import java.util.*;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		
+		System.out.println("The ADFGVXET Encryption");
+
+	} // main
+
+} // Class
